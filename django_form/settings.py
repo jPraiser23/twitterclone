5613bf0757fc5e7ba8f27e7dc535a33a1ce654f8
John Praiser <praiserjohn@gmail.com>
2022-09-07 18:54:48 -0400

    dd

--- django_form/settings.py
+++ django_form/settings.py
@@ -143,4 +143,4 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = ‘whitenoise.storage.CompressedManifestStaticFilesStorage’
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
